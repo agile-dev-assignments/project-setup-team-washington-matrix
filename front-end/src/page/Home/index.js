9b0af1e5db20ec17937d8866be5b885cac1b64eb
jw5374 <jw5374@nyu.edu>
2021-03-19 22:44:00 -0400

    fixed weird borders, fixed scrolling, updated logo

--- front-end/src/page/Home/index.js
+++ front-end/src/page/Home/index.js
@@ -6,12 +6,11 @@ import {
     Sidebar,
     Icon,
     Menu,
-    Segment,
     Grid,
     Button,
  } from 'semantic-ui-react';
 import PlayModal from '../../components/PlayModal';
-const imgsrc = '/img/chesslogo.png';
+const imgsrc = '/img/chesslogoQueen.png';
 class Home extends React.Component {
     constructor(props) {
         super(props);
@@ -28,73 +27,61 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-                <div className="rightAlign">
-                    <div style={{flex: 1}}>
-                        <Sidebar.Pushable as={Segment}>
-                            <Sidebar
-                                as={Menu}
-                                animation='overlay'
-                                icon='labeled'
-                                inverted
-                                onHide ={() => {this.toggleVisible()}}
-                                vertical
-                                visible={this.state.visible}
-                                width='thin'
-                                direction='right'
-                            >
-                                <Menu.Item as={Link} to='/'>
-                                <Icon name='chess board' />
-                                Play
-                                </Menu.Item>
-                                <Menu.Item as={Link} to='/learn'>
-                                <Icon name='pencil square' />
-                                Learn
-                                </Menu.Item>
-                                <Menu.Item as={Link} to='/practice'>
-                                <Icon name='chess' />
-                                Practice
-                                </Menu.Item>
-                                <Menu.Item as={Link} to='/info'>
-                                <Icon name='question' />
-                                Info
-                                </Menu.Item>
-                            </Sidebar>
-                            
-                            <Sidebar.Pusher dimmed={this.state.visible}>
-                                <Grid columns={3} className="background">
-                                    <Grid.Row className="columnheight">
-                                        <Grid.Column>
-                                            <Button.Group size='large' color='grey' style={{position: "relative", left: "10px"}}>
-                                                <Button as={Link} to='/login'>Log in</Button>
-                                                <Button.Or />
-                                                <Button as={Link} to='/signup'>Sign Up</Button>
-                                            </Button.Group>
-                                        </Grid.Column>
-                                        <Grid.Column></Grid.Column>
-                                        <Grid.Column floated="right" textAlign='right' >
-                                            <div>
-                                                <Image src={imgsrc} size='medium' verticalAlign='top' onClick={() => {
-                                                    this.toggleVisible();
-                                                }}/>
-                                            </div>
-                                        </Grid.Column>
-                                    </Grid.Row>
-                                    <Grid.Row centered>
-                                        <PlayModal buttonText='Play Against the Computer' />
-                                    </Grid.Row>
-                                    <Grid.Row centered>
-                                        <PlayModal buttonText='Play Locally' />
-                                    </Grid.Row>
-                                    <Grid.Row style={{height: "500px"}}>
-                                        <Grid.Column> </Grid.Column>
-                                        <Grid.Column> </Grid.Column>
-                                        <Grid.Column> </Grid.Column>
-                                    </Grid.Row>
-                                </Grid>
-                            </Sidebar.Pusher>    
-                        </Sidebar.Pushable>
-                    </div>
-                </div>
+                <Sidebar.Pushable>
+                    <Sidebar
+                        as={Menu}
+                        animation='overlay'
+                        icon='labeled'
+                        inverted
+                        onHide ={() => {this.toggleVisible()}}
+                        vertical
+                        visible={this.state.visible}
+                        width='thin'
+                        direction='right'
+                    >
+                        <Menu.Item as={Link} to='/'>
+                        <Icon name='chess board' />
+                        Play
+                        </Menu.Item>
+                        <Menu.Item as={Link} to='/learn'>
+                        <Icon name='pencil square' />
+                        Learn
+                        </Menu.Item>
+                        <Menu.Item as={Link} to='/practice'>
+                        <Icon name='chess' />
+                        Practice
+                        </Menu.Item>
+                        <Menu.Item as={Link} to='/info'>
+                        <Icon name='question' />
+                        Info
+                        </Menu.Item>
+                    </Sidebar>
+                    
+                    <Sidebar.Pusher dimmed={this.state.visible} style={{overflowY: 'scroll', height: '100vh'}}>
+                        <Grid>
+                            <Grid.Row style={{height: '50vh'}}>
+                                <Grid.Column>
+                                    <Button.Group size='large' color='grey' style={{position: 'relative', left: '10px'}}>
+                                        <Button as={Link} to='/login'>Log in</Button>
+                                        <Button.Or />
+                                        <Button as={Link} to='/signup'>Sign Up</Button>
+                                    </Button.Group>
+                                </Grid.Column>
+                                <Grid.Column floated="right" width={3}>
+                                    <Image src={imgsrc} size='medium' verticalAlign='top' onClick={() => {
+                                        this.toggleVisible();
+                                    }}/>
+                                </Grid.Column>
+                            </Grid.Row>
+                            <Grid.Row centered>
+                                <PlayModal buttonText='Play Against the Computer' />
+                            </Grid.Row>
+                            <Grid.Row centered>
+                                <PlayModal buttonText='Play Locally' />
+                            </Grid.Row>
+                        </Grid>
+                    </Sidebar.Pusher>    
+                </Sidebar.Pushable>
             </div>
         );
     }
