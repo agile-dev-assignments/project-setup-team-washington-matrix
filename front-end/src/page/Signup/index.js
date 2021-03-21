d1a5efe3abcdfc0d1ef6f57094408c7be02162c2
mzhong360 <mz1963@nyu.edu>
2021-03-21 18:04:59 -0400

    inserted forms into the signup page

--- front-end/src/page/Signup/index.js
+++ front-end/src/page/Signup/index.js
@@ -1,15 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { 
+    Form,
+    Segment,
+    Button,
+    Grid
+ } from 'semantic-ui-react';
 
 class Signup extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            visible: false,
+        }
     }
     
     render() {
         return (
-            <div>
-                Signup Page
-            </div>
+            <Grid>
+                <Grid.Row centered>
+                    <Grid.Column width={6}>
+                        <Form inverted>
+                            <Form.Field required width={6}>
+                                <label>Username</label>
+                                <input placeholder='Enter Username' />
+                            </Form.Field>
+                            <Form.Field required width = {6}>
+                                <label>Password</label>
+                                <input placeholder='Enter Password' />
+                            </Form.Field>
+                            <Form.Field required width = {6}>
+                                <label>Email</label>
+                                <input placeholder='Enter Email' />
+                            </Form.Field>
+                            <Button type='submit'>Submit</Button>
+                        </Form>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+          
+            
         )
     }
 }
