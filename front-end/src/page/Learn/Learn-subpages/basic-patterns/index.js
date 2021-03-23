62fe30b10c47d10ebce773f6b1de958e506865f0
mzhong360 <mz1963@nyu.edu>
2021-03-23 18:32:57 -0400

    added learn subpages

---
+++ front-end/src/page/Learn/Learn-subpages/basic-patterns/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Play extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div>
+                Basic Patterns
+            </div>
+        )
+    }
+}
+
+export default Play;
---
+++ front-end/src/page/Learn/Learn-subpages/mechanics/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Play extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div>
+                Mechanics
+            </div>
+        )
+    }
+}
+
+export default Play;
