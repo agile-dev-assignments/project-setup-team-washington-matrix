38f91a729035228de61a7bf33b54ce196418ca0a
Pepi <pepimartinez@protonmail.com>
2021-03-23 16:03:49 -0400

    Added login page structure

--- front-end/src/page/Login/index.js
+++ front-end/src/page/Login/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
-
+import { 
+    Form,
+    Button,
+    Grid
+ } from 'semantic-ui-react';
 class Login extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            loggedIn: false,
+        }
     }
     
     render() {
         return (
-            <div>
-                Login Page
-            </div>
+            <Grid>
+                <Grid.Row centered>
+                    <Grid.Column width={6}>
+                        <Form inverted>
+                            <Form.Field required width={6}>
+                                <label>Email</label>
+                                <input placeholder='Enter Email' />
+                            </Form.Field>
+                            <Form.Field required width = {6}>
+                                <label>Password</label>
+                                <input placeholder='Enter Password' />
+                            </Form.Field>
+                            <Button type='submit'>Submit</Button>
+                        </Form>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
         )
     }
 }
