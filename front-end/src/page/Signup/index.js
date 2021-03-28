8706d403b5ec8854ce7f04e24d4f59bd97096a8f
mzhong360 <mz1963@nyu.edu>
2021-03-28 19:04:15 -0400

    header in signup page

--- front-end/src/page/Signup/index.js
+++ front-end/src/page/Signup/index.js
@@ -6,6 +6,7 @@ import {
     Button,
     Grid
  } from 'semantic-ui-react';
+ import SidebarPerm from '../../components/SidebarPerm';
 
 class Signup extends React.Component {
     constructor(props) {
@@ -17,28 +18,38 @@ class Signup extends React.Component {
     
     render() {
         return (
-            <Grid>
-                <Grid.Row centered>
-                    <Grid.Column width={6}>
-                        <Form inverted>
-                            <Form.Field required width={6}>
-                                <label>Username</label>
-                                <input placeholder='Enter Username' />
-                            </Form.Field>
-                            <Form.Field required width = {6}>
-                                <label>Password</label>
-                                <input placeholder='Enter Password' />
-                            </Form.Field>
-                            <Form.Field required width = {6}>
-                                <label>Email</label>
-                                <input placeholder='Enter Email' />
-                            </Form.Field>
-                            <Button type='submit'>Submit</Button>
-                        </Form>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-          
+            <div>
+            <SidebarPerm 
+                buttongroup={
+                <Button.Group size='large' color='grey'>
+                    <Button as={Link} to='/login'>Log in</Button>
+                    <Button.Or />
+                    <Button as={Link} to='/signup'>Sign Up</Button>
+                </Button.Group>}
+                id="sidebarneedsstyle">
+                    <Grid>
+                        <Grid.Row centered>
+                            <Grid.Column width={6}>
+                                <Form inverted>
+                                    <Form.Field required width={6}>
+                                        <label>Username</label>
+                                        <input placeholder='Enter Username' />
+                                    </Form.Field>
+                                    <Form.Field required width = {6}>
+                                        <label>Password</label>
+                                        <input placeholder='Enter Password' />
+                                    </Form.Field>
+                                    <Form.Field required width = {6}>
+                                        <label>Email</label>
+                                        <input placeholder='Enter Email' />
+                                    </Form.Field>
+                                    <Button type='submit'>Submit</Button>
+                                </Form>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+            </SidebarPerm>
+            </div>
             
         )
     }
