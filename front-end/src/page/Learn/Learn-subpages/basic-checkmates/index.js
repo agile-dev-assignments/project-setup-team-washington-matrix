2327e6abc4b148248362baa28aeba2c3cbfbd49d
mich-han <48871474+mich-han@users.noreply.github.com>
2021-03-28 20:17:55 -0400

    prettier learn subpages

--- front-end/src/page/Learn/Learn-subpages/basic-checkmates/index.js
+++ front-end/src/page/Learn/Learn-subpages/basic-checkmates/index.js
@@ -1,62 +1,66 @@
-import React from 'react';
+import React from "react";
 import "./style.css";
-import { Link } from 'react-router-dom';
-import { 
-    Grid,
- } from 'semantic-ui-react';
-import SidebarPerm from '../../../../components/SidebarPerm';
-import WithMoveValidation from './../../../../components/boards/WithMoveValidation';
+import { Link } from "react-router-dom";
+import { Grid } from "semantic-ui-react";
+import SidebarPerm from "../../../../components/SidebarPerm";
+import WithMoveValidation from "./../../../../components/boards/WithMoveValidation";
 
+const imgsrc = "/img/chesslogoQueen.png";
 
-const imgsrc = '/img/chesslogoQueen.png';
-     
 class BasicCheckmates extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
   render() {
-        return (
-            <div>
-                
-                <SidebarPerm id="sidebarneedsstyle">
-                <h1 class= "title">
-                    Basic Checkmates  
-                </h1>
-                <div class="ui compact menu">
-                    <div class="ui simple dropdown item">
-                        Basic Checkmates    
-                        <i class="dropdown icon"></i>
-                        <div class="menu">
-                            <div class="item" data-value="1">Smothered</div>
-                            <div class="item" data-value="2">Back Rank</div>
-                            <div class="item" data-value="3">Scholars</div>
-                            <div class="item" data-value="4">Fools</div>
-                            <div class="item" data-value="5">Mate with Queen and King</div>
-                            <div class="item" data-value="6">Mate with two Rooks</div>
-                            <div class="item" data-value="7">Mate with King and Rook</div>
-                        </div>
-                    </div>
+    return (
+      <div>
+        <SidebarPerm id="sidebarneedsstyle">
+          <h1 class="title">Basic Checkmates</h1>
+          <div class="ui compact menu">
+            <div class="ui simple dropdown item">
+              Basic Checkmates
+              <i class="dropdown icon"></i>
+              <div class="menu">
+                <div class="item" data-value="1">
+                  Smothered
                 </div>
-                
-
-                <div id= "board" className="practice-container">
-                    <WithMoveValidation />
-                </div>    
-
-                <Grid className='univbackground'>
-                    <Grid.Row style={{height: '110vh'}}>
-                    </Grid.Row>
-                </Grid>
-                </SidebarPerm>  
-
-                <div id="textbox">
-                    <p>Text about the checkmates</p>
-                </div> 
-
+                <div class="item" data-value="2">
+                  Back Rank
+                </div>
+                <div class="item" data-value="3">
+                  Scholars
+                </div>
+                <div class="item" data-value="4">
+                  Fools
+                </div>
+                <div class="item" data-value="5">
+                  Mate with Queen and King
+                </div>
+                <div class="item" data-value="6">
+                  Mate with two Rooks
+                </div>
+                <div class="item" data-value="7">
+                  Mate with King and Rook
+                </div>
+              </div>
             </div>
-            
-        )
-    }
+          </div>
+
+          <div id="board" className="practice-container">
+            <WithMoveValidation />
+          </div>
+
+          <Grid className="univbackground">
+            <Grid.Row style={{ height: "110vh" }}></Grid.Row>
+          </Grid>
+        </SidebarPerm>
+
+        <div id="textbox">
+          <p>Text about the checkmates</p>
+        </div>
+      </div>
+    );
+  }
 }
 export default BasicCheckmates;
--- front-end/src/page/Learn/Learn-subpages/basic-movement/index.js
+++ front-end/src/page/Learn/Learn-subpages/basic-movement/index.js
@@ -1,60 +1,64 @@
-import React from 'react';
+import React from "react";
 import "./style.css";
-import { Link } from 'react-router-dom';
-import { 
-    Grid,
- } from 'semantic-ui-react';
-import SidebarPerm from '../../../../components/SidebarPerm';
-import WithMoveValidation from './../../../../components/boards/WithMoveValidation';
+import { Link } from "react-router-dom";
+import { Grid } from "semantic-ui-react";
+import SidebarPerm from "../../../../components/SidebarPerm";
+import WithMoveValidation from "./../../../../components/boards/WithMoveValidation";
 
-const imgsrc = '/img/chesslogoQueen.png';
-     
+const imgsrc = "/img/chesslogoQueen.png";
+
 class BasicMovements extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
   render() {
-        return (
-            <div>
-                
-                <SidebarPerm id="sidebarneedsstyle">
-                <h1 class= "title">
-                    Basic Movements 
-                </h1>
-                <div class="ui compact menu">
-                    <div class="ui simple dropdown item">
-                        Basic Movements   
-                        <i class="dropdown icon"></i>
-                        <div class="menu">
-                            <div class="item" data-value="1">King</div>
-                            <div class="item" data-value="2">Rook</div>
-                            <div class="item" data-value="3">Bishop</div>
-                            <div class="item" data-value="4">Queen</div>
-                            <div class="item" data-value="5">Knight</div>
-                            <div class="item" data-value="6">Pawn</div>
-                        </div>
-                    </div>
+    return (
+      <div>
+        <SidebarPerm id="sidebarneedsstyle">
+          <h1 class="title">Basic Movements</h1>
+          <div class="ui compact menu">
+            <div class="ui simple dropdown item">
+              Basic Movements
+              <i class="dropdown icon"></i>
+              <div class="menu">
+                <div class="item" data-value="1">
+                  King
                 </div>
-
-                <div id= "board" className="practice-container">
-                    <WithMoveValidation />
-                </div>                
-                
-                <Grid className='univbackground'>
-                    <Grid.Row style={{height: '110vh'}}>
-                    </Grid.Row>
-                </Grid>
-                </SidebarPerm>
-
-                <div id="infotext">
-                    <p>Text about the movements</p>
+                <div class="item" data-value="2">
+                  Rook
                 </div>
-                
+                <div class="item" data-value="3">
+                  Bishop
+                </div>
+                <div class="item" data-value="4">
+                  Queen
+                </div>
+                <div class="item" data-value="5">
+                  Knight
+                </div>
+                <div class="item" data-value="6">
+                  Pawn
+                </div>
+              </div>
             </div>
-            
-        )
-    }
+          </div>
+
+          <div id="board" className="practice-container">
+            <WithMoveValidation />
+          </div>
+
+          <Grid className="univbackground">
+            <Grid.Row style={{ height: "110vh" }}></Grid.Row>
+          </Grid>
+        </SidebarPerm>
+
+        <div id="infotext">
+          <p>Text about the movements</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default BasicMovements;
