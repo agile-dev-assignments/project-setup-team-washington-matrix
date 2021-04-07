d141c03a4d6283efa0870d31c783754a3904db35
jw5374 <jw5374@nyu.edu>
2021-04-06 21:01:58 -0400

    fixed styling on puzzles page

--- front-end/src/page/Learn/Learn-subpages/puzzles/index.js
+++ front-end/src/page/Learn/Learn-subpages/puzzles/index.js
@@ -1,51 +1,39 @@
-import React from "react";
-import "./style.css";
-import { Link } from "react-router-dom";
-import { Grid, Button } from "semantic-ui-react";
-import SidebarPerm from "../../../../components/SidebarPerm";
-import WithMoveValidation from "./../../../../components/boards/WithMoveValidation";
+import React from 'react';
+import './style.css';
+import { Link } from 'react-router-dom';
+import { Grid, Button } from 'semantic-ui-react';
+import SidebarPerm from '../../../../components/SidebarPerm';
+import WithMoveValidation from './../../../../components/boards/WithMoveValidation';
 
-const imgsrc = "/img/chesslogoQueen.png";
+const imgsrc = '/img/chesslogoQueen.png';
 
 class Puzzles extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-      <div>
-        <SidebarPerm id="sidebarneedsstyle">
-          <h1 id="title">Puzzles</h1>
-          buttongroup=
-          {
-            <Button.Group id="difficulty" size="large" color="grey">
-              <Button>
-                Easy
-              </Button>
-              <Button>
-                Medium
-              </Button>
-              <Button>
-                Hard
-              </Button>
-            </Button.Group>
-          }
-          <Grid>
-            <Grid.Row centered>
-              <Grid.Column width={6}>
-                <WithMoveValidation />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-
-          <Grid className="univbackground">
-            <Grid.Row style={{ height: "100vh" }}></Grid.Row>
-          </Grid>
-        </SidebarPerm>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <SidebarPerm id="sidebarneedsstyle">
+                    <h1 id="title">Puzzles</h1>
+                    <Grid className="univbackground">
+                        <Grid.Row centered>
+                            <Button.Group size="large" color="grey">
+                                <Button>Easy</Button>
+                                <Button>Medium</Button>
+                                <Button>Hard</Button>
+                            </Button.Group>
+                        </Grid.Row>
+                        <Grid.Row centered>
+                            <WithMoveValidation />
+                        </Grid.Row>
+                        <Grid.Row style={{ height: '40vh' }}></Grid.Row>
+                    </Grid>
+                </SidebarPerm>
+            </div>
+        );
+    }
 }
 
 export default Puzzles;
