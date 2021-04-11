96a91f3d6240cbe914d327acc43ba65f72325e78
Alex Bobb <ab7253@nyu.edu>
2021-04-10 20:41:56 -0700

    loaded positions into the mechanics page

--- front-end/src/page/Learn/Learn-subpages/mechanics/index.js
+++ front-end/src/page/Learn/Learn-subpages/mechanics/index.js
@@ -1,59 +1,124 @@
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './style.css';
-import { Link } from 'react-router-dom';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Dropdown, Container } from 'semantic-ui-react';
 import Layout from '../../../../components/Layout';
 import WithMoveValidation from './../../../../components/boards/WithMoveValidation';
 
-const imgsrc = '/img/chesslogoQueen.png';
+function CastlingText(props) {
+    const [data, setData] = useState('');
+    useEffect(() => {
+        axios.get('http://localhost:4000/learn/mechanics/castling').then((res) => {
+            setData(res.data.data);
+        });
+        return () => {
+            setData('');
+        };
+    }, []);
+    return <p>{data.pieceInfo}</p>;
+}
 
-class Mechanics extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+function EnpassantText(props) {
+    const [data, setData] = useState('');
+    useEffect(() => {
+        axios.get('http://localhost:4000/learn/mechanics/enpassant').then((res) => {
+            setData(res.data.data);
+        });
+        return () => {
+            setData('');
+        };
+    }, []);
+    return <p>{data.pieceInfo}</p>;
+}
 
-    render() {
-        return (
-            <div>
-                <Layout id="sidebarneedsstyle">
-                    <h1 class="title">Mechanics</h1>
-                    <div class="ui compact menu">
-                        <div class="ui simple dropdown item">
-                            Mechanics
-                            <i class="dropdown icon"></i>
-                            <div class="menu">
-                                <div class="item" data-value="1">
-                                    Castling
-                                </div>
-                                <div class="item" data-value="2">
-                                    En Passant
-                                </div>
-                                <div class="item" data-value="3">
-                                    Pawn Promotion
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+function PawnpromotionText(props) {
+    const [data, setData] = useState('');
+    useEffect(() => {
+        axios.get('http://localhost:4000/learn/mechanics/pawnpromotion').then((res) => {
+            setData(res.data.data);
+        });
+        return () => {
+            setData('');
+        };
+    }, []);
+    return <p>{data.pieceInfo}</p>;
+}
 
-                    <Grid>
-                        <Grid.Row centered>
-                            <Grid.Column width={6}>
-                                <WithMoveValidation />
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
 
-                    <Grid className="univbackground">
-                        <Grid.Row style={{ height: '110vh' }}></Grid.Row>
-                    </Grid>
-                </Layout>
-
-                <div id="infotext">
-                    <p>Text about the mechanics</p>
-                </div>
-            </div>
-        );
+function DisplayedText(props) {
+    const piece = props.piece;
+    switch (piece) {
+        case 1:
+            return <CastlingText />;
+        case 2:
+            return <EnpassantText />;
+        case 3:
+            return <PawnpromotionText />;
+        default:
+            return <CastlingText />;
     }
 }
 
+const pieces = [
+    {
+        key: 1,
+        text: 'Castling',
+        value: 1,
+        fen: '8/8/8/8/4P3/1PN5/PBPQ1PPP/R3KBNR w - - 0 1',
+    },
+    {
+        key: 2,
+        text: 'En Passant',
+        value: 2,
+        fen: 'rnbqkb1r/ppppp1pp/8/3nPp2/3P4/8/PPP2PPP/RNBQKBNR w KQkq f6 0 4',
+    },
+    {
+        key: 3,
+        text: 'Pawn Promotion',
+        value: 3,
+        fen: '2r5/1P6/8/8/8/8/8/8 w - - 0 1',
+    },
+   
+];
+
+const Mechanics = () => {
+    const handleDropdownClick = async (event, data) => {
+        console.log(data);
+        setPieceSelected(data.value);
+        setPieceBoard(pieces.find(({ value }) => value === data.value).fen);
+    };
+    const [pieceSelected, setPieceSelected] = useState();
+    const [pieceBoard, setPieceBoard] = useState(pieces.find(({ text }) => text === 'Castling').fen);
+    console.log(pieceBoard);
+    return (
+        <div>
+            <Layout id="sidebarneedsstyle">
+                <h1 class="title">Mechanics</h1>
+
+                <Grid className="univbackground">
+                    <Grid.Row centered>
+                        <Dropdown
+                            placeholder="Select Option"
+                            selection
+                            options={pieces}
+                            onChange={handleDropdownClick}
+                        />
+                    </Grid.Row>
+                    <Grid.Row centered>
+                        <WithMoveValidation setFen={pieceBoard} />
+                    </Grid.Row>
+                    <Grid.Row centered>
+                        <Container text>
+                            <DisplayedText piece={pieceSelected} />
+                        </Container>
+                    </Grid.Row>
+                    <Grid.Row style={{ height: '50vh' }}></Grid.Row>
+                </Grid>
+            </Layout>
+        </div>
+    );
+};
+
 export default Mechanics;
+
