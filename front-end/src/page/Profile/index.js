e04406c60175f325cb498cee5230f2b42f39959d
Pepi <pepimartinez@protonmail.com>
2021-04-25 22:54:22 -0400

    converted profilepage to functional component

--- front-end/src/page/Profile/index.js
+++ front-end/src/page/Profile/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import profilepic from './profile.png';
 import { Grid, Image } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Edit from '../../components/EditModal';
+// import authService from '../../services/authService';
 
 const Line = () => (
     <hr
@@ -15,75 +16,66 @@ const Line = () => (
     />
 );
 
-class Profile extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            opengame: false,
-            openpuzzle: false,
-        };
-        this.toggleg = this.toggleg.bind(this);
-        this.togglep = this.togglep.bind(this);
-    }
+const Profile = (props) => {
+    const [openGame, setopenGame] = useState(false);
+    const [openPuzzle, setopenPuzzle] = useState(false);
 
-    toggleg(game) {
-        this.setState({ opengame: !this.state.opengame });
-    }
+    //     this.toggleg = this.toggleg.bind(this);
+    //     this.togglep = this.togglep.bind(this);
+    // }
 
-    togglep(puzzle) {
-        this.setState({ openpuzzle: !this.state.openpuzzle });
-    }
+    const toggleOpenGame = () => {
+        setopenGame(!openGame);
+    };
 
-    render() {
-        return (
-            <div>
-                <Layout id="sidebarneedsstyle">
-                    <div>
-                        <div id="edit">
-                            <Edit />
-                        </div>
-                        <Image id="propic" src={profilepic} alt="ProfilePic" />
-                        <div>
-                            <h2 id="user">Username</h2>
-                            <h4 id="name">Name</h4>
-                        </div>
+    const toggleOpenPuzzle = () => {
+        setopenPuzzle(!openPuzzle);
+    };
+
+    return (
+        <div>
+            <Layout id="sidebarneedsstyle">
+                <div>
+                    <div id="edit">
+                        <Edit />
                     </div>
-                    <Line />
+                    <Image id="propic" src={profilepic} alt="ProfilePic" />
                     <div>
-                        <div name="game" onClick={(game) => this.toggleg(game)} className="header">
-                            Games
-                        </div>
-                        {this.state.opengame ? (
-                            <div className="content">
-                                <h4>Total Games Played: 48</h4>
-                                <h4>Wins: 32</h4>
-                                <h4>Losses: 14</h4>
-                            </div>
-                        ) : null}
+                        <h2 id="user">Username</h2>
+                        <h4 id="name">Name</h4>
                     </div>
+                </div>
+                <Line />
+                <div>
+                    <div name="game" onClick={toggleOpenGame} className="header">
+                        Games
+                    </div>
+                    {openGame ? (
+                        <div className="content">
+                            <h4>Total Games Played: 48</h4>
+                            <h4>Wins: 32</h4>
+                            <h4>Losses: 14</h4>
+                        </div>
+                    ) : null}
+                </div>
 
-                    <div>
-                        <div
-                            name="puzzle"
-                            onClick={(puzzle) => this.togglep(puzzle)}
-                            className="header"
-                        >
-                            Puzzles
-                        </div>
-                        {this.state.openpuzzle ? (
-                            <div className="content">
-                                <h4>Total Puzzles Completed: 152</h4>
-                            </div>
-                        ) : null}
-                        {/* i'm assuming we will have more info about statistics */}
+                <div>
+                    <div name="puzzle" onClick={toggleOpenPuzzle} className="header">
+                        Puzzles
                     </div>
-                </Layout>
-                <Grid className="univbackground">
-                    <Grid.Row style={{ height: '50vh' }}></Grid.Row>
-                </Grid>
-            </div>
-        );
-    }
-}
+                    {openPuzzle ? (
+                        <div className="content">
+                            <h4>Total Puzzles Completed: 152</h4>
+                        </div>
+                    ) : null}
+                    {/* i'm assuming we will have more info about statistics */}
+                </div>
+            </Layout>
+            <Grid className="univbackground">
+                <Grid.Row style={{ height: '50vh' }}></Grid.Row>
+            </Grid>
+        </div>
+    );
+};
 
 export default Profile;
