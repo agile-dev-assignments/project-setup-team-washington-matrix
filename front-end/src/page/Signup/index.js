775418f7887e2fa4de81bed2037e8483352daa06
Pepi <pepimartinez@protonmail.com>
2021-04-25 22:41:54 -0400

    added form validation

--- front-end/src/page/Signup/index.js
+++ front-end/src/page/Signup/index.js
@@ -1,40 +1,66 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Header, Form, Button, Grid, Image, Segment, Message } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Header, Button, Grid, Image, Segment, Message } from 'semantic-ui-react';
 import './style.css';
 import Layout from '../../components/Layout';
+import axios from 'axios';
+import { Formik } from 'formik';
+import { Form, Input, SubmitButton, ResetButton } from 'formik-semantic-ui-react';
 
-const Signup = () => (
-    <Layout id="sidebarneedsstyle">
-        <Grid textAlign="center" style={{ height: '73vh' }} verticalAlign="middle">
-            <Grid.Column style={{ maxWidth: 450 }}>
-                <Header as="h2" color="teal" textAlign="center">
-                    <Image src="/img/chesslogo.png" /> Create a new account
-                </Header>
-                <Form size="large">
-                    <Segment stacked>
-                        <Form.Input fluid icon="user" iconPosition="left" placeholder="Username" />
-                        <Form.Input
-                            fluid
-                            icon="envelope outline"
-                            iconPosition="left"
-                            placeholder="Email"
-                        />
-                        <Form.Input
-                            fluid
-                            icon="lock"
-                            iconPosition="left"
-                            placeholder="Password"
-                            type="password"
-                        />
-                        <Button color="teal" fluid size="large" type="submit">
-                            Signup
-                        </Button>
-                    </Segment>
-                </Form>
-            </Grid.Column>
-        </Grid>
-    </Layout>
-);
+import * as Yup from 'yup';
+
+const SignupSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Required'),
+    password: Yup.string().min(7, 'Too Short!').max(50, 'Too Long!').required('Required'),
+    password2: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match'),
+});
+
+const Signup = () => {
+    const initialValues = {
+        email: '',
+        password: '',
+        password2: '',
+    };
+    return (
+        <Layout id="sidebarneedsstyle">
+            <Grid textAlign="center" style={{ height: '73vh' }} verticalAlign="middle">
+                <Grid.Column style={{ maxWidth: 450 }}>
+                    <Header as="h2" color="teal" textAlign="center">
+                        <Image src="/img/chesslogo.png" /> Create a new account
+                    </Header>
+                    <Formik
+                        initialValues={initialValues}
+                        validationSchema={SignupSchema}
+                        onSubmit={(values) => {
+                            console.log('Form Submit');
+                            console.log(values);
+                        }}
+                    >
+                        <Form size="large">
+                            <Input name="email" placeholder="Email" errorPrompt />
+                            <Input
+                                name="password"
+                                type="password"
+                                placeholder="Password"
+                                errorPrompt
+                            />
+                            <Input
+                                name="password2"
+                                type="password"
+                                placeholder="Confirm Password"
+                                errorPrompt
+                            />
+                            <SubmitButton fluid color="teal">
+                                Login
+                            </SubmitButton>
+                            <ResetButton fluid secondary>
+                                Reset Form
+                            </ResetButton>
+                        </Form>
+                    </Formik>
+                </Grid.Column>
+            </Grid>
+        </Layout>
+    );
+};
 
 export default Signup;
